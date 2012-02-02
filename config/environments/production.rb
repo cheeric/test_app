82e33135d16c4c23ad41854394100786a9e8ece0
Eric Chen <eric10759@gmail.com>
2012-02-01 22:17:47 -0500

    Homework 1 & 2, third time's a charm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 TestApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.assets.precompile += %w( app.js foundation.js app.css ie.css foundation.css )
 
   # Code is not reloaded between requests
   config.cache_classes = true
