e7b51f48d4b28a69a72cdb87a42c6cc3ce51dfb8
Eric Chen <eric10759@gmail.com>
2012-04-29 04:55:10 -0400

    changed from has_one to has_many

--- db/migrate/20120220040604_create_users.rb
+++ db/migrate/20120220040604_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.has_one :cart
+      t.has_many :cart
 
       t.timestamps
     end
